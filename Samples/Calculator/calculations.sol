cb53b383ddf0cf866ea101ecda057556c5502c85
Vatsavaye priyatham Varma <53737699+PriyathamVarma@users.noreply.github.com>
2022-12-03 19:52:50 +0000

    Create calculations.sol

---
+++ Samples/Calculator/calculations.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+/*  The gas will be high for the contract
+    This is not an optimised contract
+    The reason is because it is for learning the concepts not for production
+*/        
+
+contract calculations {
+
+    // Variables
+    int sum;
+    int sub;
+    int mul;
+    int div;
+    int pow;
+
+    // Functions
+
+    // Adding function
+    // setter
+    function setAdd(int _a,int _b)
+            public{
+
+                sum = _a + _b;
+
+            }
+
+    // getter
+    function getAdd()
+                view
+                public
+                returns(int){
+                    return sum;
+                }
+
+
+    // subtracting function
+    // setter
+    function setSub(int _a,int _b)
+            public{
+
+                sub = _a - _b;
+
+            }
+
+    // getter
+    function getSub()
+                view
+                public
+                returns(int){
+                    return sub;
+                }                    
+
+
+    // multiplying function
+    // setter
+    function setMul(int _a,int _b)
+            public{
+
+                mul = _a * _b;
+
+            }
+
+    // getter
+    function getMul()
+                view
+                public
+                returns(int){
+                    return mul;
+                }  
+
+
+
+    // dividing function
+    // setter
+    function setDiv(int _a,int _b)
+            public{
+
+                div = _a / _b;
+
+            }
+
+    // getter
+    function getDiv()
+                view
+                public
+                returns(int){
+                    return div;
+                }  
+
+
+    // powerOf function
+    // setter
+    function setPow(int _a,uint _b)// You cant have the power of in "-"
+            public{
+
+                pow = _a ** _b;// _a^_b
+
+            }
+
+    // getter
+    function getPow()
+                view
+                public
+                returns(int){
+                    return pow;
+                }                          
+                
+
+
+}
